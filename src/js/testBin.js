bc5a66fbf2bcfda060ca34e203f7166dc4f4c601
t-schultz <tschultztt@gmail.com>
2015-09-19 13:20:32 -0500

    update

--- src/js/testBin.js
+++ src/js/testBin.js
@@ -104,38 +104,91 @@ var teFlow = require('../../lib/te-flow');
 // };
 
 
+// var merge = function() {
+//     var obj = {},
+//         i = 0,
+//         il = arguments.length,
+//         key;
+//     for (; i < il; i++) {
+//         for (key in arguments[i]) {
+//             if (arguments[i].hasOwnProperty(key)) {
+//                 obj[key] = arguments[i][key];
+//             }
+//         }
+//     }
+//     return obj;
+// };
+
+
+
+// var one = function () {
+//   return {
+//     keyOne: 1
+//   };
+// };
+// var two = function (oneVal) {
+//   //oneVal {keyOne: 1}
+//   return merge(arguments, {keyTwo: 2});
+// };
+// var three = function (oneVal, twoVal) {
+//   //oneVal {keyOne: 1}
+//   //twoVal {keyTwo: 2}
+//   return merge(arguments, {keyThree: 3});
+// };
+
+
+// var res = teFlow(
+//     {
+//       _objKeep: true
+//     },
+//     one,
+//     two,
+//     three
+// );
+
+// res === [1, 2, 4];
+
+
+// var addNewObjArg = function (args, newObj) {
+//   // args.push(newObj);
+//   return Object.keys(newObj).reduce(function (prv, curKey) {
+//     debugger
+//     prv[curKey] = newObj[curKey];
+//     return prv;
+//   }, args);
+// };
+
 var merge = function() {
-    var obj = {},
-        i = 0,
-        il = arguments.length,
-        key;
-    for (; i < il; i++) {
-        for (key in arguments[i]) {
-            if (arguments[i].hasOwnProperty(key)) {
-                obj[key] = arguments[i][key];
-            }
-        }
+  var obj = {},
+      i = 0,
+      il = arguments.length,
+      key;
+  for (; i < il; i++) {
+    for (key in arguments[i]) {
+      if (arguments[i].hasOwnProperty(key)) {
+          obj[key] = arguments[i][key];
+      }
     }
-    return obj;
+  }
+  return obj;
 };
-
-
 
 var one = function () {
   return {
     keyOne: 1
   };
 };
+
 var two = function (oneVal) {
   //oneVal {keyOne: 1}
-  return merge(arguments, {keyTwo: 2});
+  return merge(oneVal, {keyTwo: 2});
 };
+
 var three = function (oneVal, twoVal) {
   //oneVal {keyOne: 1}
   //twoVal {keyTwo: 2}
-  return merge(arguments, {keyThree: 3});
+  return merge(oneVal, twoVal, {keyThree: 3});
 };
-
 
 var res = teFlow(
     {
@@ -145,8 +198,6 @@ var res = teFlow(
     two,
     three
 );
-
-// res === [1, 2, 4];
 
 
 debugger
